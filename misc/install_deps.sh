467ac489f0b0942ed06c808ca2324bdc5586f84e
mthh <mattshageo@autistici.org>
2016-08-29 12:01:26 +0200

    Fix mistake in setup

--- misc/install_deps.sh
+++ misc/install_deps.sh
@@ -4,8 +4,9 @@ set -ex
 
 if [ ! -d "$OTHER_PY_PACKAGES" ]; then
   mkdir $OTHER_PY_PACKAGES;
+fi
+  cd $OTHER_PY_PACKAGES
   git clone http://github.com/mthh/smoomapy
   cd smoomapy/
   python setup.py install
-fi
 cd $TRAVIS_BUILD_DIR
