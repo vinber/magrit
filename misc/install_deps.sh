66000a12f85c1076d033afec9d48438fdaf3eb58
mthh <mattshageo@autistici.org>
2016-08-29 12:23:56 +0200

    fix install scripts

--- misc/install_deps.sh
+++ misc/install_deps.sh
@@ -6,6 +6,7 @@ if [ ! -d "$OTHER_PY_PACKAGES" ]; then
   mkdir $OTHER_PY_PACKAGES;
 fi
   cd $OTHER_PY_PACKAGES
+  rm -rf smoomapy
   git clone http://github.com/mthh/smoomapy
   cd smoomapy/
   pip install -r requirements.txt
