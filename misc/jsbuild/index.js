b3de25f848cbd5d406d4e882baa3f53990652e10
mthh <mattshageo@autistici.org>
2016-10-14 13:01:08 +0200

    Add minified js file

--- misc/jsbuild/index.js
+++
@@ -1,7 +0,0 @@
-import * as foo from "../../noname_app/static/js/function";
-import click_button_add_layer from "../../noname_app/static/js/interface";
-export default {
-	click_button_add_layer: click_button_add_layer,
-	get_menu_option: get_menu_option
-}
-
