vinber/magrit

commit 18e3ad7ed88440eac4809c57b042d5edc9871054

Lint code

magrit_app/static/js/join_popup.js
@@ -1,17 +1,15 @@
-"use strict";
-
-function handle_join(){
+function handleJoin() {
   const layer_name = Object.getOwnPropertyNames(user_data);
 
   if (!(layer_name.length === 1 && joined_dataset.length === 1)) {
     swal('', i18next.t('app_page.join_box.unable_join'), 'error');
-  } else if(field_join_map.length != 0){
+  } else if (field_join_map.length != 0) {
     make_confirm_dialog2('dialogBox', undefined, { html_content: i18next.t('app_page.join_box.ask_forget_join') })
       .then((confirmed) => {
         if (confirmed) {
           valid_join_check_display();
           field_join_map = [];
-          remove_existing_jointure(layer_name);
+          removeExistingJointure(layer_name);
           createJoinBox(layer_name[0]);
         }
       });
@@ -26,43 +24,43 @@ function handle_join(){
 }
 
 // Function called to update the menu according to user operation (triggered when layers/dataset are added and after a join operation)
-function valid_join_check_display(val, prop){
+function valid_join_check_display(val, prop) {
   if (!val) {
-    let ext_dataset_img = document.getElementById("img_data_ext");
-    ext_dataset_img.setAttribute("src", "/static/img/b/joinfalse.png");
-    ext_dataset_img.setAttribute("alt", "Non-validated join");
-    ext_dataset_img.style.width = "28px";
-    ext_dataset_img.style.height = "28px";
-    ext_dataset_img.onclick = handle_join;
+    const extDatasetImg = document.getElementById('img_data_ext');
+    extDatasetImg.setAttribute('src', '/static/img/b/joinfalse.png');
+    extDatasetImg.setAttribute('alt', 'Non-validated join');
+    extDatasetImg.style.width = '28px';
+    extDatasetImg.style.height = '28px';
+    extDatasetImg.onclick = handleJoin;
 
-    let join_sec = document.getElementById("join_section");
-    join_sec.innerHTML = [prop, i18next.t('app_page.join_box.state_not_joined')].join('');
+    const joinSec = document.getElementById('join_section');
+    joinSec.innerHTML = [prop, i18next.t('app_page.join_box.state_not_joined')].join('');
 
-    let button = document.createElement("button");
-    button.setAttribute("id", "join_button");
-    button.style.display = "inline";
-    button.innerHTML = '<button style="font-size: 11px;" class="button_st3" id="_join_button">' + i18next.t("app_page.join_box.button_join") + '</button>'
-    button.onclick = handle_join;
-    join_sec.appendChild(button);
+    const button = document.createElement('button');
+    button.setAttribute('id', 'join_button');
+    button.style.display = 'inline';
+    button.innerHTML = `<button style="font-size: 11px;" class="button_st3" id="_join_button">${i18next.t('app_page.join_box.button_join')}</button>`;
+    button.onclick = handleJoin;
+    joinSec.appendChild(button);
   } else {
-    let ext_dataset_img = document.getElementById("img_data_ext");
-    ext_dataset_img.setAttribute("src", "/static/img/b/jointrue.png");
-    ext_dataset_img.setAttribute("alt", "Validated join");
-    ext_dataset_img.style.width = "28px";
-    ext_dataset_img.style.height = "28px";
-    ext_dataset_img.onclick = null;
+    const extDatasetImg = document.getElementById('img_data_ext');
+    extDatasetImg.setAttribute('src', '/static/img/b/jointrue.png');
+    extDatasetImg.setAttribute('alt', 'Validated join');
+    extDatasetImg.style.width = '28px';
+    extDatasetImg.style.height = '28px';
+    extDatasetImg.onclick = null;
 
-    let [v1, v2] = prop.split("/").map(d => +d);
+    const [v1, _] = prop.split('/').map(d => +d);
 
-    let join_sec = document.getElementById("join_section");
-    join_sec.innerHTML = [' <b>', prop, i18next.t("app_page.join_box.match", {count: v1}), '</b>'].join(' ');
+    const joinSec = document.getElementById('join_section');
+    joinSec.innerHTML = [' <b>', prop, i18next.t('app_page.join_box.match', { count: v1 }), '</b>'].join(' ');
 
-    let button = document.createElement("button");
-    button.setAttribute("id", "join_button");
-    button.style.display = "inline";
-    button.innerHTML = [" - <i> ", i18next.t("app_page.join_box.change_field"), " </i>"].join('');
-    button.onclick = handle_join;
-    join_sec.appendChild(button);
+    const button = document.createElement('button');
+    button.setAttribute('id', 'join_button');
+    button.style.display = 'inline';
+    button.innerHTML = [' - <i> ', i18next.t('app_page.join_box.change_field'), ' </i>'].join('');
+    button.onclick = handleJoin;
+    joinSec.appendChild(button);
   }
 }
 
@@ -71,63 +69,59 @@ function valid_join_check_display(val, prop){
 //    -the update of the global "field_join_map" array
 //       (storing the relation between index of the geometry layer and index of the external dataset)
 //    -the update of the global "user_data" object, adding actualy the value to each object representing each feature of the layer
-function valid_join_on(layer_name, field1, field2){
-  var join_values1 = [],
-      join_values2 = [],
-      hits = 0,
-      val = undefined;
+function valid_join_on(layer_name, field1, field2) {
+  let join_values1 = [],
+    join_values2 = [],
+    hits = 0,
+    val;
 
   field_join_map = [];
 
-  for (let i=0, len=joined_dataset[0].length; i<len; i++) {
+  for (let i = 0, len = joined_dataset[0].length; i < len; i++) {
     join_values2.push(joined_dataset[0][i][field2]);
   }
-  for (let i=0, len=user_data[layer_name].length; i<len; i++) {
+  for (let i = 0, len = user_data[layer_name].length; i < len; i++) {
     join_values1.push(user_data[layer_name][i][field1]);
   }
 
   if (has_duplicate(join_values1) || has_duplicate(join_values2)) {
-    return swal("", i18next.t("app_page.join_box.error_not_uniques"), "warning");
+    return swal('', i18next.t('app_page.join_box.error_not_uniques'), 'warning');
   }
 
-  if(typeof join_values1[0] === "number" && typeof join_values2[0] === "string"){
-    for(let i=0, len=join_values1.length; i<len; i++){
+  if (typeof join_values1[0] === 'number' && typeof join_values2[0] === 'string') {
+    for (let i = 0, len = join_values1.length; i < len; i++) {
       val = join_values2.indexOf(String(join_values1[i]));
-      if(val != -1) { field_join_map.push(val); hits++; }
-      else { field_join_map.push(undefined); }
+      if (val != -1) { field_join_map.push(val); hits++; } else { field_join_map.push(undefined); }
     }
-  } else if(typeof join_values2[0] === "number" && typeof join_values1[0] === "string"){
-    for(let i=0, len=join_values1.length; i<len; i++){
+  } else if (typeof join_values2[0] === 'number' && typeof join_values1[0] === 'string') {
+    for (let i = 0, len = join_values1.length; i < len; i++) {
       val = join_values2.indexOf(Number(join_values1[i]));
-      if(val != -1) { field_join_map.push(val); hits++; }
-      else { field_join_map.push(undefined); }
+      if (val != -1) { field_join_map.push(val); hits++; } else { field_join_map.push(undefined); }
     }
-  } else if(typeof join_values2[0] === "number" && typeof join_values1[0] === "number"){
-    for(let i=0, len=join_values1.length; i<len; i++){
+  } else if (typeof join_values2[0] === 'number' && typeof join_values1[0] === 'number') {
+    for (let i = 0, len = join_values1.length; i < len; i++) {
       val = join_values2.indexOf(join_values1[i]);
-      if(val != -1) { field_join_map.push(val); hits++; }
-      else { field_join_map.push(undefined); }
+      if (val != -1) { field_join_map.push(val); hits++; } else { field_join_map.push(undefined); }
     }
   } else {
-    for(let i=0, len=join_values1.length; i<len; i++){
+    for (let i = 0, len = join_values1.length; i < len; i++) {
       val = join_values2.indexOf(String(join_values1[i]));
-      if(val != -1) { field_join_map.push(val); hits++; }
-      else { field_join_map.push(undefined); }
+      if (val != -1) { field_join_map.push(val); hits++; } else { field_join_map.push(undefined); }
     }
   }
 
-  var prop = [hits, "/", join_values1.length].join(""),
-      f_name = "";
+  let prop = [hits, '/', join_values1.length].join(''),
+    f_name = '';
 
   if (hits == join_values1.length) {
-    swal({title: '',
-          text: i18next.t('app_page.common.success'),
-          type: "success",
-          allowOutsideClick: true});
-    let fields_name_to_add = Object.getOwnPropertyNames(joined_dataset[0][0]);
-    for (let i=0, len=join_values1.length; i<len; i++) {
+    swal({ title: '',
+      text: i18next.t('app_page.common.success'),
+      type: 'success',
+      allowOutsideClick: true });
+    const fields_name_to_add = Object.getOwnPropertyNames(joined_dataset[0][0]);
+    for (let i = 0, len = join_values1.length; i < len; i++) {
       val = field_join_map[i];
-      for (let j=0, leng=fields_name_to_add.length; j<leng; j++) {
+      for (let j = 0, leng = fields_name_to_add.length; j < leng; j++) {
         f_name = fields_name_to_add[j];
         if (f_name.length > 0) {
           user_data[layer_name][i][f_name] = joined_dataset[0][val][f_name];
@@ -138,54 +132,53 @@ function valid_join_on(layer_name, field1, field2){
     return Promise.resolve(true);
   } else if (hits > 0) {
     return swal({
-      title: i18next.t("app_page.common.confirm") + "!",
-      text: i18next.t("app_page.join_box.partial_join", { ratio: prop }),
+      title: `${i18next.t('app_page.common.confirm')}!`,
+      text: i18next.t('app_page.join_box.partial_join', { ratio: prop }),
       allowOutsideClick: false,
       allowEscapeKey: true,
-      type: "question",
+      type: 'question',
       showConfirmButton: true,
       showCancelButton: true,
-      confirmButtonText: i18next.t("app_page.common.yes"),
-      cancelButtonText: i18next.t("app_page.common.no"),
+      confirmButtonText: i18next.t('app_page.common.yes'),
+      cancelButtonText: i18next.t('app_page.common.no'),
     }).then(() => {
-      let fields_name_to_add = Object.getOwnPropertyNames(joined_dataset[0][0])
+      const fields_name_to_add = Object.getOwnPropertyNames(joined_dataset[0][0]);
       for (let i = 0, len = field_join_map.length; i < len; i++) {
         val = field_join_map[i];
-        for (let j=0, leng=fields_name_to_add.length; j<leng; j++) {
+        for (let j = 0, leng = fields_name_to_add.length; j < leng; j++) {
           f_name = fields_name_to_add[j];
           if (f_name.length > 0) {
             let t_val;
             if (val == undefined) t_val = null;
             else if (joined_dataset[0][val][f_name] === '') t_val = null;
             else t_val = joined_dataset[0][val][f_name];
-            user_data[layer_name][i][f_name] = val != undefined ? joined_dataset[0][val][f_name] : null ;
+            user_data[layer_name][i][f_name] = val != undefined ? joined_dataset[0][val][f_name] : null;
           }
         }
       }
       valid_join_check_display(true, prop);
       return Promise.resolve(true);
-    }, dismiss => {
+    }, (dismiss) => {
       field_join_map = [];
       return Promise.resolve(false);
     });
-  } else {
-    swal('',
-         i18next.t('app_page.join_box.no_match', { field1: field1, field2: field2 }),
-         "error");
-    field_join_map = [];
-    return Promise.resolve(false);
   }
+  swal('',
+       i18next.t('app_page.join_box.no_match', { field1, field2 }),
+       'error');
+  field_join_map = [];
+  return Promise.resolve(false);
 }
 
 // Function creating the join box , filled by to "select" input linked one to
 // the geometry layer and the other to the external dataset, in order to choose
 // the common field to do the join.
 const createJoinBox = function createJoinBox(layer) {
-  var geom_layer_fields = [...current_layers[layer].original_fields.keys()],
-      ext_dataset_fields = Object.getOwnPropertyNames(joined_dataset[0][0]),
-      button1 = ["<select id=button_field1>"],
-      button2 = ["<select id=button_field2>"],
-      last_choice = {"field1": geom_layer_fields[0], "field2": ext_dataset_fields[0]};
+  const geom_layer_fields = [...current_layers[layer].original_fields.keys()];
+  const ext_dataset_fields = Object.getOwnPropertyNames(joined_dataset[0][0]);
+  const button1 = ['<select id=button_field1>'];
+  const button2 = ['<select id=button_field2>'];
+  const lastChoice = { field1: geom_layer_fields[0], field2: ext_dataset_fields[0] };
 
   for (let i = 0, len = geom_layer_fields.length; i < len; i++) {
     button1.push(['<option value="', geom_layer_fields[i], '">', geom_layer_fields[i], '</option>'].join(''));
@@ -199,7 +192,7 @@ const createJoinBox = function createJoinBox(layer) {
   }
   button2.push('</select>');
 
-  let inner_box = [
+  const inner_box = [
     '<p><b><i>',
     i18next.t('app_page.join_box.select_fields'), '</i></b></p>',
     '<div style="padding:10px"><p>',
@@ -208,32 +201,32 @@ const createJoinBox = function createJoinBox(layer) {
     '<div style="padding:15px 10px 10px"><p>',
     i18next.t('app_page.join_box.ext_dataset_field'), '<br></p>',
     button2.join(''), '<em style="float:right;">(', dataset_name, '.csv)</em></div>',
-    '<br><p><strong>', i18next.t('app_page.join_box.ask_join'), '<strong></p></div>'
+    '<br><p><strong>', i18next.t('app_page.join_box.ask_join'), '<strong></p></div>',
   ].join('');
 
 
   make_confirm_dialog2('joinBox', i18next.t('app_page.join_box.title'), { html_content: inner_box, widthFitContent: true })
     .then((confirmed) => {
       if (confirmed) {
-        valid_join_on(layer, last_choice.field1, last_choice.field2)
+        valid_join_on(layer, lastChoice.field1, lastChoice.field2)
             .then((valid) => { if (valid) make_box_type_fields(layer); });
       }
     });
 
   d3.select('.joinBox').styles({ 'text-align': 'center', 'line-height': '0.9em' });
-  d3.select('#button_field1').style('float', 'left').on('change', function(){ last_choice.field1 = this.value; });
-  d3.select('#button_field2').style('float', 'left').on('change', function(){ last_choice.field2 = this.value; });
+  d3.select('#button_field1').style('float', 'left').on('change', () => { lastChoice.field1 = this.value; });
+  d3.select('#button_field2').style('float', 'left').on('change', () => { lastChoice.field2 = this.value; });
 };
 
-const remove_existing_jointure = (layer_name) => {
+const removeExistingJointure = (layer_name) => {
   if (!user_data[layer_name] || user_data[layer_name].length < 1) return;
-  const data_layer = user_data[layer_name];
+  const dataLayer = user_data[layer_name];
   const original_fields = current_layers[layer_name].original_fields;
-  const field_difference = Object.getOwnPropertyNames(data_layer[0]).filter(f => !original_fields.has(f));
-  const nbFields = field_difference.length;
-  for (let i = 0, nbFt = data_layer.length; i < nbFt; i++) {
+  const fieldDifference = Object.getOwnPropertyNames(dataLayer[0]).filter(f => !original_fields.has(f));
+  const nbFields = fieldDifference.length;
+  for (let i = 0, nbFt = dataLayer.length; i < nbFt; i++) {
     for (let j = 0; j < nbFields; j++) {
-      delete data_layer[i][field_difference[j]];
+      delete dataLayer[i][fieldDifference[j]];
     }
   }
 };

magrit_app/templates/modules.html
@@ -7,7 +7,7 @@
     <title> {{ app_name }} </title>
     <link rel="icon" type="image/png" href="static/img/favicon.png" />
     <link href="static/css/scoped-bootstrap.min.css" rel="stylesheet" type="text/css">
-	<link href="static/css/style.b54df6aa9f19.min.css" rel="stylesheet"  type="text/css">
+	<link href="static/css/style.b64089af101b.min.css" rel="stylesheet"  type="text/css">
     <link href="static/css/vanilla-dataTables.min.css" rel="stylesheet" type="text/css">
     <link href="static/css/alertify.min.css" rel="stylesheet" type="text/css">
     <link href="static/css/semantic.min.css" rel="stylesheet" type="text/css">
@@ -51,6 +51,6 @@
         </div>
       </noscript>
     <script  src="static/js/lib/bootstrap-native3.mod.min.js"></script>
-	<script  src="static/js/app.b54df6aa9f19.js"></script>
+	<script  src="static/js/app.b64089af101b.js"></script>
 </body>
 </html>

commit 315923f94eec494411c31bf2e8875b1de7168abc

Fix failing jointure (introduced in 18e3ad7e)

magrit_app/static/js/app.fb440288f7d7.js
@@ -1092,7 +1092,7 @@ function parseQuery(search) {
     lng: lang,
     fallbackLng: _app.existing_lang[0],
     backend: {
-      loadPath: 'static/locales/{{lng}}/translation.c14ce4938e6e.json'
+      loadPath: 'static/locales/{{lng}}/translation.fb440288f7d7.json'
     }
   }, function (err, tr) {
     if (err) {
@@ -8736,12 +8736,10 @@ function click_button_add_layer() {
 * @param {FileList} files - The files(s) to be handled for this layer
 * @param {Boolean} target_layer_on_add - wether this is the target layer or not
 * @param {HTMLElement} elem - Optionnal The parent element on which the file was dropped
-* @param {String} type - Optionnal, the type (GeoJson, Topojson or project file) of the
-* file if it is a file in JSON format.
 *
 * @return undefined
 */
-function handle_upload_files(files, target_layer_on_add, elem, type) {
+function handle_upload_files(files, target_layer_on_add, elem) {
   var tot_size = Array.prototype.map.call(files, function (f) {
     return f.size;
   }).reduce(function (a, b) {
@@ -10748,8 +10746,6 @@ function valid_join_on(layer_name, field1, field2) {
 // the geometry layer and the other to the external dataset, in order to choose
 // the common field to do the join.
 var createJoinBox = function createJoinBox(layer) {
-  var _this = this;
-
   var geom_layer_fields = [].concat(_toConsumableArray(current_layers[layer].original_fields.keys()));
   var ext_dataset_fields = Object.getOwnPropertyNames(joined_dataset[0][0]);
   var button1 = ['<select id=button_field1>'];
@@ -10780,10 +10776,10 @@ var createJoinBox = function createJoinBox(layer) {
 
   d3.select('.joinBox').styles({ 'text-align': 'center', 'line-height': '0.9em' });
   d3.select('#button_field1').style('float', 'left').on('change', function () {
-    lastChoice.field1 = _this.value;
+    lastChoice.field1 = this.value;
   });
   d3.select('#button_field2').style('float', 'left').on('change', function () {
-    lastChoice.field2 = _this.value;
+    lastChoice.field2 = this.value;
   });
 };
 
@@ -15961,7 +15957,7 @@ function apply_user_preferences(json_pref) {
       a.querySelector('button').style.display = '';
       var targeted_layer = Object.getOwnPropertyNames(user_data)[0];
       if (targeted_layer) getAvailablesFunctionnalities(targeted_layer);
-    }, 125);
+    }, 200);
   };
 
   function apply_layout_lgd_elem() {

magrit_app/static/js/join_popup.js
@@ -209,13 +209,15 @@ const createJoinBox = function createJoinBox(layer) {
     .then((confirmed) => {
       if (confirmed) {
         valid_join_on(layer, lastChoice.field1, lastChoice.field2)
-            .then((valid) => { if (valid) make_box_type_fields(layer); });
+          .then((valid) => {
+            if (valid) make_box_type_fields(layer);
+          });
       }
     });
 
   d3.select('.joinBox').styles({ 'text-align': 'center', 'line-height': '0.9em' });
-  d3.select('#button_field1').style('float', 'left').on('change', () => { lastChoice.field1 = this.value; });
-  d3.select('#button_field2').style('float', 'left').on('change', () => { lastChoice.field2 = this.value; });
+  d3.select('#button_field1').style('float', 'left').on('change', function() { lastChoice.field1 = this.value; });
+  d3.select('#button_field2').style('float', 'left').on('change', function() { lastChoice.field2 = this.value; });
 };
 
 const removeExistingJointure = (layer_name) => {

magrit_app/templates/modules.html
@@ -7,7 +7,7 @@
     <title> {{ app_name }} </title>
     <link rel="icon" type="image/png" href="static/img/favicon.png" />
     <link href="static/css/scoped-bootstrap.min.css" rel="stylesheet" type="text/css">
-	<link href="static/css/style.c14ce4938e6e.min.css" rel="stylesheet"  type="text/css">
+	<link href="static/css/style.fb440288f7d7.min.css" rel="stylesheet"  type="text/css">
     <link href="static/css/vanilla-dataTables.min.css" rel="stylesheet" type="text/css">
     <link href="static/css/alertify.min.css" rel="stylesheet" type="text/css">
     <link href="static/css/semantic.min.css" rel="stylesheet" type="text/css">
@@ -51,6 +51,6 @@
         </div>
       </noscript>
     <script  src="static/js/lib/bootstrap-native3.mod.min.js"></script>
-	<script  src="static/js/app.c14ce4938e6e.js"></script>
+	<script  src="static/js/app.fb440288f7d7.js"></script>
 </body>
 </html>

tests/tests_web.py
@@ -315,6 +315,8 @@ class ProjectRoundTrip(TestBase):
         with open(self.tmp_folder + 'magrit_project.json') as f:
             data_2 = json.loads(f.read())
 
+        del data_1['info']['version']
+        del data_2['info']['version']
         # Are the two projects the same ?
         assertDeepAlmostEqual(self, data_1, data_2, "Simple Map 1")
 
@@ -404,7 +406,7 @@ class ReloadCompareProjectsTest(TestBase):
 
     def t_reload(self, i, name):
         url, b_image, project = \
-            self.urls[name], self.images[name], self.projects[name]
+            self.urls[name], self.images[name], json.loads(self.projects[name])
         with self.subTest(i=i):
             driver = self.driver
             driver.get(self.base_url + "?reload={}".format(url))
@@ -433,7 +435,9 @@ class ReloadCompareProjectsTest(TestBase):
             time.sleep(2.5)
             with open(self.tmp_folder + 'magrit_project.json') as f:
                 data = json.loads(f.read())
-            assertDeepAlmostEqual(self, data, json.loads(project), name)
+            del data['info']['version']
+            del project['info']['version']
+            assertDeepAlmostEqual(self, data, project, name)
             os.remove(self.tmp_folder + 'magrit_project.json')
 
     def test_each_project(self):