89c19c63e24513853457dced0c104e1f4bee02cc
mthh <matthieu.viry@cnrs.fr>
2018-04-25 11:58:03 +0200

    wip changelog translation

--- client/post_build.sh
+++ client/post_build.sh
@@ -1,4 +1,5 @@
 echo "Replacing files in /magrit_app/static/dist/ folder..."
+mkdir -p ../magrit_app/static/dist
 rm -rf ../magrit_app/static/dist/app.js
 cp dist/app.js ../magrit_app/static/dist/app.js
 cp css/style-fonts.css ../magrit_app/static/dist/style-fonts.css
