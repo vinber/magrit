aee68bb2320d085ebdf37afb6f9dfc5c9ddbc5e9
mthh <matthieu.viry@cnrs.fr>
2018-04-26 12:21:11 +0200

    wip

--- misc/bump_version.py
+++ misc/bump_version.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import json
 import os
 import sys
+from subprocess import Popen, PIPE
+
 
 def get_version():
     with open('__init__.py', 'r') as f:
@@ -12,7 +15,25 @@ def get_version():
 
 def save_version(version):
     with open('__init__.py', 'w') as f:
-        f.write("# -*- coding: utf-8 -*-\n\n__version__ = '{}'\n".format(version))
+        f.write("# -*- coding: utf-8 -*-\n\n__version__ = '{}'\n"
+                .format(version))
+
+
+def save_version_packagejson(version):
+    with open('package.json') as f:
+        data = json.loads(f.read())
+    data['version'] = version
+    with open('package.json', 'w') as f:
+        f.write(json.dumps(data, indent=2))
+
+
+def rebuild_assets():
+    p = Popen(
+        'NODE_ENV=production ./node_modules/webpack/bin/webpack.js',
+        shell=True, stderr=PIPE, stdout=PIPE)
+    a, b = p.communicate()
+    if b:
+        print(b)
 
 
 if __name__ == '__main__':
@@ -33,5 +54,12 @@ if __name__ == '__main__':
         current_version[1] = 0
         current_version[0] += 1
     current_version_txt = '.'.join(map(str, current_version))
+    print('Magrit\n', old_version, ' -> ', current_version_txt, '\n')
+    print('- Replacing version number in __init__.py ...')
     save_version(current_version_txt)
-    print(old_version, ' -> ', current_version_txt)
+    os.chdir('../client')
+    print('- Replacing version number in package.json ...')
+    save_version_packagejson(current_version_txt)
+    print('- Building minified JS/CSS files ...')
+    rebuild_assets()
+    print('\nDone! ', old_version, ' -> ', current_version_txt)
